7a0769f5f8031c0c53d26b4fefc26b2063ed6fa7
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2018-03-30 17:34:14 +0100

    Update vagrant-setup.sh

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -1,2 +1,3 @@
 #https://blog.sleeplessbeastie.eu/2016/08/01/how-to-get-started-with-vagrant/
 #https://vegibit.com/how-to-provision-nginx-using-vagrant/
+#http://www.bogotobogo.com/DevOps/Vagrant/Vagrant_VirtualBox.php
